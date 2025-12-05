3560f605558a3ac1c92778c0213e949926feaba6
Oleksandr Liashkov <aleksandr.liashkov98@gmail.com>
2025-12-05 12:53:58 +0200

    Task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleLoginForm);
+
+function handleLoginForm(event) {
+  event.preventDefault();
+
+  const emailField = loginForm.elements.email;
+  const passwordField = loginForm.elements.password;
+
+  const emailValue = emailField.value.trim();
+  const passwordValue = passwordField.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+}
